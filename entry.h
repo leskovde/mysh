2c7cb6dab9f17208f9356850bcb563a3a00c9208
leskovde <47887202+leskovde@users.noreply.github.com>
2020-01-13 16:15:26 +0100

    refactoring

--- entry.h
+++ entry.h
@@ -1,24 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-#define nullptr NULL
-
-struct env
-{
-        char * actual_path;
-        char * previous_path;
-        char * prompt_text;
-        int return_val;
-        bool file_run;
-};
-
-struct env env_;
-
-int
-quit();
-
-void
-free_resources();
 
 int
 launcher(int, char **);
