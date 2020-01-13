7639f5b9a929ab2980bdfe69caf7236714785ab3
leskovde <47887202+leskovde@users.noreply.github.com>
2020-01-13 16:17:50 +0100

    refactoring + added comments

---
+++ signals.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include "signals.h"
+
+/**
+ * Redraws the prompt after getting interrupted when a command is running.
+ */
+void
+interrupt_cmd(int signo)
+{
+        if (signo == SIGINT)
+        {
+                rl_on_new_line_with_prompt();
+                rl_replace_line("", 0);
+                rl_redisplay();
+        }
+}
+
+/**
+ * Redraws the prompt after getting interrupted in it by the user.
+ */
+void
+interrupt_prompt(int signo)
+{
+        if (signo == SIGINT)
+        {
+                rl_crlf();
+                rl_on_new_line();
+                rl_replace_line("", 0);
+                rl_redisplay();
+        }
+}
